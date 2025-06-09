89f8bce307cda0649e094be123d2d23661621feb
Yas Patle <you@example.com>
2025-06-09 17:06:44 +0530

    minor change

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Made with 💗 by RoadsideCoder
+       Made by Yas Patle
       </div>
     </div>
   );
